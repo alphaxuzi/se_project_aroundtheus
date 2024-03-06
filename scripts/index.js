8b6a271df7e673ba5a04fc4a3b94d9ef97903277
Alpha Bah <alphabah2395@gmail.com>
2024-03-06 15:12:53 -0500

    Project 4 task 1

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+let initialCards = [
+    { name: "Shibuya Streets", link: "https://unsplash.com/photos/people-gathered-outside-buildings-and-vehicles-alY6_OpdwRQ" },
+    { name: "Malibu Beach", link: "https://unsplash.com/photos/people-on-seashore-during-daytime-hVdQfUA2asA" },
+    { name: "Las Vegas", link: "https://unsplash.com/photos/the-las-vegas-strip-at-night-with-the-eiffel-tower-in-the-background-PYaP4awj9Uc" },
+    { name: "African Elephants", link: "https://unsplash.com/photos/elephants-on-grass-during-daytime-NZHU5vfPo3M" },
+    { name: "Arctic Fox", link: "https://unsplash.com/photos/selective-focus-photography-of-gray-wolf-on-snow-field-V__yW0wQCOs" },
+    { name: "Great Wall of China", link: "https://unsplash.com/photos/a-view-of-the-great-wall-of-china-in-the-fog-80OWrQGTtG8" }
+]
