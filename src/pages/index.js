ad3f275357502cac8551480a174c2fd748856a64
Alpha Bah <alphabah2395@gmail.com>
2024-05-20 03:20:32 -0400

    Project 8

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,8 +18,6 @@ function handleImageClick(cardData) {
   popupWithImage.open(cardData);
 }
 
-const cardListEl = document.querySelector(".cards__list");
-
 const section = new Section(
   {
     items: initialCards,
@@ -46,8 +44,6 @@ const profileEditModal = document.querySelector("#edit-profile");
 const profileForm = profileEditModal.querySelector(".modal__form");
 const modalTitleInput = document.querySelector("#modalTitle");
 const modalDescriptionInput = document.querySelector("#modalDescription");
-// const profileTitle = document.querySelector(".profile__title");
-// const profileDescription = document.querySelector(".profile__description");
 
 const popupProfileEdit = new PopupWithForm(
   "#edit-profile",
@@ -69,7 +65,6 @@ profileEditButton.addEventListener("click", () => {
 function handleProfileFormSubmit({ title, description }) {
   userInfo.setUserInfo({ name: title, job: description });
   popupProfileEdit.close();
-  profileForm.reset();
   validateProfile.toggleButtonState();
 }
 
@@ -100,16 +95,5 @@ const validateAddPlace = new FormValidator(config, addPlaceForm);
 validateAddPlace.enableValidation();
 
 const popupWithImage = new PopupWithImage({ popupSelector: "#image-modal" });
-
-// Cards Section
-// const cardTitleInput = document.querySelector("#modal-title");
-// const cardUrlInput = document.querySelector("#modal-description");
-
-//  initialCards.forEach((cardData) => {
-// const cardElement = createCard(cardData);
-// cardListEl.prepend(cardElement);
-// });
-
-// section.renderItems();
 
 popupWithImage.setEventListeners();
